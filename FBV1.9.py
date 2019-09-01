860dd68d47e407368ba2f7cad3c042c115f28906
baaztian <54778391+baaztian@users.noreply.github.com>
2019-09-02 03:28:07 +0700

    Update FBV1.9.py

--- FBV1.9.py
+++ FBV1.9.py
@@ -34,23 +34,7 @@ def jalan(z):
         time.sleep(0.01)
 
 
-# Variables
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-blue='\e[1;34m'
-cyan='\e[1;36m'
-red='\e[1;31m'
 
-figlet BAZTIAN | lolcat
 
 echo -e  "____________________________________________________________"
 echo -e  "Tools    : FacebookV1.9 $white         " |lolcat
